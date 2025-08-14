cff55c04df5bb1c60799fb5cdd5db01fdfe23c7b
David Dangyil <daviddangyil@gmail.com>
2025-08-14 11:52:35 +0100

    more changes

--- src/succcespage.jsx
+++ src/succcespage.jsx
@@ -2,7 +2,7 @@ export default function SuccessPage() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Form Submitted Successfully!</h1>
-      <p>Welcome to the next page.</p>
+      <button onClick={() => window.location.href = "/"}>Go back</button>
     </div>
   );
 }
